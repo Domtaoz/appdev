cd3a4c082923075e22b4521dfffe1f127b383b9b
Pollapat <129392645+Domtaoz@users.noreply.github.com>
2025-02-20 04:14:46 +0000

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,51 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const uri = 'https://didactic-space-invention-69v6vxv7gx72r4pq-5001.app.github.dev/';
+
+  
+  const fetchBooks = async () => {
+    try {
+      const respine = await axios.get(`${uri}/books`);
+      setBooks(respine.data.books);  
+    } catch (error) {
+      console.error('Error fetching books:', error); 
+    }
+  };
+
+  
+  useEffect(() => {
+    fetchBooks();
+  }, []); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Book List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr key={book.id}>
+              <td>{book.id}</td>
+              <td><img src={book.image_url} alt={book.title} width="50" /></td>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td><button>Edit</button><button>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
